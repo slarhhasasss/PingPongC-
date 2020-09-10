f3874a62b29256e9ae6bd5622d526f3480b77d7b
slarhhasasss <dimikmusik@yandex.ru>
2020-09-10 16:02:03 +0300

    Hey it is my game

---
+++ PingPongForTwo/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace PingPongForTwo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameField = new System.Windows.Forms.Panel();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.textBoxScore = new System.Windows.Forms.TextBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.leftPictureBox = new System.Windows.Forms.PictureBox();
+            this.rightPictureBox = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.gameField.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.leftPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rightPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gameField
+            // 
+            this.gameField.Controls.Add(this.btnExit);
+            this.gameField.Controls.Add(this.btnStart);
+            this.gameField.Controls.Add(this.textBoxScore);
+            this.gameField.Controls.Add(this.ball);
+            this.gameField.Controls.Add(this.leftPictureBox);
+            this.gameField.Controls.Add(this.rightPictureBox);
+            this.gameField.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gameField.Location = new System.Drawing.Point(0, 0);
+            this.gameField.Name = "gameField";
+            this.gameField.Size = new System.Drawing.Size(800, 450);
+            this.gameField.TabIndex = 0;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnExit.Location = new System.Drawing.Point(435, 40);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(100, 50);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.onClickExit);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnStart.Location = new System.Drawing.Point(309, 40);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(120, 50);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.onClickStart);
+            // 
+            // textBoxScore
+            // 
+            this.textBoxScore.CausesValidation = false;
+            this.textBoxScore.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBoxScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxScore.Location = new System.Drawing.Point(0, 0);
+            this.textBoxScore.Name = "textBoxScore";
+            this.textBoxScore.Size = new System.Drawing.Size(800, 34);
+            this.textBoxScore.TabIndex = 3;
+            this.textBoxScore.Text = "0:0";
+            this.textBoxScore.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ball.Location = new System.Drawing.Point(466, 204);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(30, 30);
+            this.ball.TabIndex = 2;
+            this.ball.TabStop = false;
+            // 
+            // leftPictureBox
+            // 
+            this.leftPictureBox.BackColor = System.Drawing.Color.Red;
+            this.leftPictureBox.Location = new System.Drawing.Point(164, 66);
+            this.leftPictureBox.Name = "leftPictureBox";
+            this.leftPictureBox.Size = new System.Drawing.Size(25, 150);
+            this.leftPictureBox.TabIndex = 1;
+            this.leftPictureBox.TabStop = false;
+            // 
+            // rightPictureBox
+            // 
+            this.rightPictureBox.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.rightPictureBox.Location = new System.Drawing.Point(295, 236);
+            this.rightPictureBox.Name = "rightPictureBox";
+            this.rightPictureBox.Size = new System.Drawing.Size(25, 150);
+            this.rightPictureBox.TabIndex = 0;
+            this.rightPictureBox.TabStop = false;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 10;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gameField);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.gameField.ResumeLayout(false);
+            this.gameField.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.leftPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rightPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel gameField;
+        private System.Windows.Forms.TextBox textBoxScore;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox leftPictureBox;
+        private System.Windows.Forms.PictureBox rightPictureBox;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ PingPongForTwo/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PingPongForTwo
+{
+
+    public partial class Form1 : Form
+    {
+        //Скорости мячика
+        private int speed_vertical_ball = 3;
+        private int speed_horizontal_ball = 3;
+        //счет слева и справа
+        private int score_left = 0;
+        private int score_right = 0;
+        //скорости передвижения панелек
+        private int speed_right_panel = 5;
+        private int speed_left_panel = 5;
+
+        //определители, была ли нажата  та или иная клавиша
+        private int iAPBtn_l_ctrl = 0;
+        private int iAPBtn_r_ctrl = 0;
+        private int iAPBtn_l_shift = 0;
+        private int iAPBtn_r_shift = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //запускаем таймер
+            //gameTimer.Enabled = true;
+
+            //Прячем курсор в окне
+            //Cursor.Hide();
+
+            //Устанавливаем отсутствие рамок
+            this.FormBorderStyle = FormBorderStyle.None;
+            
+            //наше приложение будет выводиться поверх всех остальных
+            this.TopMost = true;
+
+            //Устанавливаем размер окна на весь экран
+            Bounds = Screen.PrimaryScreen.Bounds;
+
+            //Устанавливаем, что нельзя трогать это поле, оно будет изменяться программно
+            textBoxScore.Enabled = false;
+
+            //задаем положение правой и левой панельки
+            rightPictureBox.Left = gameField.Right - 50;
+            leftPictureBox.Left = 25;
+
+            //Положение кнопки
+            btnStart.Left = gameField.Width / 2 - 50;
+            btnExit.Left = btnStart.Right + 10;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch(e.KeyCode)
+            {
+                //выходим из прилолжения
+                case Keys.Escape:
+                    this.Close();
+                    break;
+                //прописываем действия при нажатии на клавиши
+                case Keys.Z:
+                    iAPBtn_l_ctrl = 1;
+                    break;
+                case Keys.M:
+                    iAPBtn_r_ctrl = 1;
+                    break;
+                case Keys.A:
+                    iAPBtn_l_shift = 1;
+                    break;
+                case Keys.K:
+                    iAPBtn_r_shift = 1;
+                    break;
+                case Keys.Enter:
+                    startGame();
+                    break;
+            }
+        }
+
+
+        //Функция таймер
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            //отслеживаем изменение положения наших панелей для отбивания мячика
+            changePositionOfPannels();
+
+
+            //movement of ball:
+            ball.Left += speed_horizontal_ball;
+            ball.Top += speed_vertical_ball;
+
+            //если касается верха или низа, то изменяет направление
+            if(ball.Top >= gameField.Top)
+            {
+                speed_vertical_ball *= -1;
+            }
+
+            if (ball.Bottom <= gameField.Bottom)
+            {
+                speed_vertical_ball *= -1;
+            }
+
+            //Если касается экрана слева
+            if(ball.Left <= gameField.Left)
+            {
+                score_right += 1;
+                stopGame();
+            }
+            //Right
+            if(ball.Right >= gameField.Right)
+            {
+                score_left += 1;
+                stopGame();
+            }
+
+            //отслеживаем касания наших панелей для отбивания мяча
+            if(ball.Right <= rightPictureBox.Right && ball.Right >= rightPictureBox.Left 
+                && ball.Top >= rightPictureBox.Top && ball.Bottom <= rightPictureBox.Bottom)
+            {
+                speed_horizontal_ball *= -1;
+
+                //Change background to random color
+                Random randColor = new Random();
+                gameField.BackColor = Color.FromArgb(randColor.Next(150, 255), randColor.Next(150, 255), randColor.Next(150, 255));
+            }
+            if(ball.Left >= leftPictureBox.Left && ball.Left <= leftPictureBox.Right
+                && ball.Top >= leftPictureBox.Top && ball.Bottom <= leftPictureBox.Bottom)
+            {
+                //Меняем его направление
+                speed_horizontal_ball *= -1;
+                //Ускоряем мячик
+                if (speed_horizontal_ball < 0) speed_horizontal_ball--;
+                else speed_horizontal_ball++;
+
+                if (speed_vertical_ball < 0) speed_vertical_ball--;
+                else speed_vertical_ball++;
+                
+                //Change background to random color
+                Random randColor = new Random();
+                gameField.BackColor = Color.FromArgb(randColor.Next(150, 255), randColor.Next(150, 255), randColor.Next(150, 255));
+            }
+
+        }
+
+        private void stopGame()
+        {
+            gameTimer.Enabled = false;
+            textBoxScore.Text = $"{score_left} : {score_right}";
+            btnStart.Enabled = true;
+            btnStart.Visible = true;
+            Cursor.Show();
+            btnExit.Enabled = true;
+            btnExit.Visible = true;
+
+            speed_horizontal_ball = 3 * speed_horizontal_ball / Math.Abs(speed_horizontal_ball);
+            speed_vertical_ball = 3 * speed_vertical_ball / Math.Abs(speed_vertical_ball);
+
+            speed_horizontal_ball *= -1;
+        }
+
+        //Изменяем позиции наших панелей для отбивания
+        private void changePositionOfPannels()
+        {
+            if(iAPBtn_l_ctrl == 1)
+            {
+                if(leftPictureBox.Bottom - speed_left_panel <= gameField.Bottom)
+                    leftPictureBox.Top += speed_left_panel;
+                //iAPBtn_l_ctrl = 0;
+            }
+            if(iAPBtn_r_ctrl == 1)
+            {
+                if (rightPictureBox.Bottom - speed_right_panel <= gameField.Bottom)
+                    rightPictureBox.Top += speed_right_panel;
+                //iAPBtn_r_ctrl = 0;
+            }
+            if(iAPBtn_l_shift == 1)
+            {
+                if (leftPictureBox.Top + speed_left_panel >= gameField.Top)
+                    leftPictureBox.Top -= speed_left_panel;
+                //iAPBtn_l_shift = 0;
+            }
+            if(iAPBtn_r_shift == 1)
+            {
+                if (rightPictureBox.Top - speed_right_panel >= gameField.Top)
+                    rightPictureBox.Top -= speed_left_panel;
+                //iAPBtn_r_shift = 0;
+            }
+        }
+
+
+        //Обрабатываем отпускание клавиш
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch(e.KeyCode)
+            {
+                //прописываем действия при отжатии клавиш
+                case Keys.Z:
+                    iAPBtn_l_ctrl = 0;
+                    break;
+                case Keys.M:
+                    iAPBtn_r_ctrl = 0;
+                    break;
+                case Keys.A:
+                    iAPBtn_l_shift = 0;
+                    break;
+                case Keys.K:
+                    iAPBtn_r_shift = 0;
+                    break;
+            }
+        }
+
+        private void onClickStart(object sender, EventArgs e)
+        {
+            startGame();
+        }
+
+        private void startGame()
+        {
+            btnStart.Visible = false;
+            btnStart.Enabled = false;
+            //start timer
+            gameTimer.Enabled = true;
+            gameTimer.Start();
+            //move ball to center
+            ball.Left = Screen.PrimaryScreen.Bounds.Width / 2;
+            //hide cursor
+            Cursor.Hide();
+            btnExit.Enabled = false;
+            btnExit.Visible = false;
+        }
+
+        private void onClickExit(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
